eb212f11dd489664fe3d73a5f8be89a94e6fb31c
ValeriSplye <lerka.elizarova@bk.ru>
2023-04-27 12:41:17 +0500

    Доделал CRUD пользователей, Начала работу над авторизацией

--- src/mvc/app/app.component.ts
+++ src/mvc/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ServiceDeskECTS';
+
+  constructor(public router:Router){
+
+  }
+
+  ngOnInit(): void {
+    console.log(this.router.url);
+  }
 }
